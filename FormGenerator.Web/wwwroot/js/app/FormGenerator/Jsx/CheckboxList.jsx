6c8005140c22432de1fcfcfa320ae69e3c7165e2
svoruganti <svoruganti@gmail.com>
2017-04-20 19:15:24 +0100

    Update CheckboxList.jsx
    Added rendering mechanism and click event

--- FormGenerator.Web/wwwroot/js/app/FormGenerator/Jsx/CheckboxList.jsx
+++ FormGenerator.Web/wwwroot/js/app/FormGenerator/Jsx/CheckboxList.jsx
@@ -1,9 +1,25 @@
 class CheckboxList extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {value : new Set()}
+    }
     render() {
+        let json = this.props.children === undefined ? [] : JSON.parse(this.props.children);
+        let boxes = json.map((item, index) => {
+            return (<ReactBootstrap.Checkbox value={item.Id} onClick={this.handleClick.bind(this)} inline>{item.Description}</ReactBootstrap.Checkbox>
+        });
         return (
             <ReactBootstrap.FormGroup controlId={this.props.code}>
-                <input type="check" id={this.props.id} name={this.props.id}/>
+                <ReactBootstrap.ControlLabel>{this.props.label}</ReactBootstrap.ControlLabel>
+                {boxes}
             </ReactBootstrap.FormGroup>
         );
+    }
+    handleClick(e){
+        if (this.state.value.has(e.target.value)
+            this.state.value.delete(e.target.value);
+        else
+            this.state.value.add(e.target.value);
+        store.dispatch({type: BRANCHING, payload:{controlId:this.props.code, value: this.state.value}};
     }
 }
