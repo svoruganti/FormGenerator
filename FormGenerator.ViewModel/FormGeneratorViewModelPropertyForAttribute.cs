15bf41c6294cb7c089563fd2fe74ebdff2dc39e8
svoruganti <svoruganti@gmail.com>
2017-04-20 14:25:42 +0100

    Create FormGeneratorViewModelPropertyForAttribute.cs
    Added form generator viewmodel attribute for

---
+++ FormGenerator.ViewModel/FormGeneratorViewModelPropertyForAttribute.cs
@@ -0,0 +1,7 @@
+public class FormGeneratorViewModelPropertyForAttribute{
+  public FormGeneratorViewModelPropertyForAttribute(string propertyName){
+    PropertName = propertyName;
+  } 
+  
+  public string PropertyName { get; private set;}
+}
